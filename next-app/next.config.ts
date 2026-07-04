35dd054c22987fedf1bfb5fccb12559ee2f03455
Adam Basile <adam.basile@readytech.io>
2026-07-04 22:05:20 +1000

    Configure Next.js static export and GitHub Pages deployment

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: process.env.PAGES_BASE_PATH,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
