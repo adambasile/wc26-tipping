ba1aa782b52acbb2615ac6352e4eb23a5438fba3
Adam Basile <basile.adam@gmail.com>
2026-07-04 21:48:40 +1000

    Getting started on a website

---
+++ next-app/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Container } from "@mui/material";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Container>{children}</Container>
+      </body>
+    </html>
+  );
+}
